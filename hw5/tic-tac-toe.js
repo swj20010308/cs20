18d458eb5c719d49f40e928f234e670ee629f8a8
swj20010308 <swj20010308@outlook.com>
2023-02-23 02:02:51 -0500

    style

--- hw5/tic-tac-toe.js
+++ hw5/tic-tac-toe.js
@@ -1,148 +1,159 @@
 const player = { X: 'X', O: 'O' }
 
-
-function Player(name) {
-    this.name = name;
+function Player (name) {
+    this.name = name
 
     this.nextPlayer = () => {
         if (this.name == player.X) {
-            return player.O;
+            return player.O
         } else {
-            return player.X;
+            return player.X
         }
-    };
+    }
 
     this.move = (game, i) => {
-        game.board[i] = this.name;
-        document.getElementById("index" + i).innerHTML = this.name;
-        document.getElementById("prompt").innerHTML =
-            "Player " + this.nextPlayer() + "'s turn:";
-    };
+        game.board[i] = this.name
+        document.getElementById('index' + i).innerHTML = this.name
+        document.getElementById('prompt').innerHTML =
+            'Player ' + this.nextPlayer() + "'s turn:"
+    }
 }
 
-function Game(dim, startPlayer) {
-    this.currentPlayer = startPlayer;
+function Game (dim, startPlayer) {
+    this.currentPlayer = startPlayer
 
-    this.board = new Array(dim * dim).fill("Empty");
+    this.board = new Array(dim * dim).fill('Empty')
 
     this.clean = () => {
-        this.board.fill("Empty");
-        this.board.forEach((v, i) =>
-            document.getElementById("index" + i).innerHTML = "");
-        this.currentPlayer = startPlayer;
-    };
+        this.board.fill('Empty')
+        this.board.forEach(
+            (v, i) => (document.getElementById('index' + i).innerHTML = '')
+        )
+        this.currentPlayer = startPlayer
+    }
 
-    this.isEmpty = (i) => this.board[i] == "Empty";
+    this.isEmpty = i => this.board[i] == 'Empty'
 
     this.displayCurrentPlayer = () =>
-        document.getElementById("prompt").innerHTML =
-        "Player " + this.currentPlayer.name + "'s turn:";
+        (document.getElementById('prompt').innerHTML =
+            'Player ' + this.currentPlayer.name + "'s turn:")
 
-    this.checkRow = (i) => {
-        let row = Math.floor(i / dim);
-        return this.board.slice(row * dim, (row + 1) * dim).reduce((b, v, i) => b && (v == this.currentPlayer.name), true);
+    this.checkRow = i => {
+        let row = Math.floor(i / dim)
+        return this.board
+            .slice(row * dim, (row + 1) * dim)
+            .reduce((b, v, i) => b && v == this.currentPlayer.name, true)
     }
 
-    this.checkCol = (i) => {
-        let col = i % dim;
-        return this.board.filter((v, i) => i % dim == col).reduce((b, v, i) => b && (v == this.currentPlayer.name), true);
+    this.checkCol = i => {
+        let col = i % dim
+        return this.board
+            .filter((v, i) => i % dim == col)
+            .reduce((b, v, i) => b && v == this.currentPlayer.name, true)
     }
 
-    this.checkDiag = (i) => {
-
-        let diag1 = new Array(0);
-        let j = i;
-        while (j < this.board.length - 1) { j += (dim + 1); }
+    this.checkDiag = i => {
+        let diag1 = new Array(0)
+        let j = i
+        while (j < this.board.length - 1) {
+            j += dim + 1
+        }
         if (j == this.board.length - 1) {
             while (j >= 0) {
-                diag1.push(this.board[j]);
-                j -= (dim + 1);
-                console.log(j);
+                diag1.push(this.board[j])
+                j -= dim + 1
+                console.log(j)
             }
         }
 
-        let diag2 = new Array(0);
-        let k = i;
-        console.log(k);
-        while (k < this.board.length - dim) { k += (dim - 1); console.log(k); }
+        let diag2 = new Array(0)
+        let k = i
+        console.log(k)
+        while (k < this.board.length - dim) {
+            k += dim - 1
+            console.log(k)
+        }
         if (k == this.board.length - dim) {
             while (k >= dim - 1) {
-                diag2.push(this.board[k]);
-                k -= (dim - 1);
-                console.log(k);
+                diag2.push(this.board[k])
+                k -= dim - 1
+                console.log(k)
             }
         }
 
-        let checkDiag1 = diag1.reduce((b, v, i) => b && (v == this.currentPlayer.name), true) && diag1.length == dim;
-        let checkDiag2 = diag2.reduce((b, v, i) => b && (v == this.currentPlayer.name), true) && diag2.length == dim;
-        return checkDiag1 || checkDiag2;
+        let checkDiag1 =
+            diag1.reduce(
+                (b, v, i) => b && v == this.currentPlayer.name,
+                true
+            ) && diag1.length == dim
+        let checkDiag2 =
+            diag2.reduce(
+                (b, v, i) => b && v == this.currentPlayer.name,
+                true
+            ) && diag2.length == dim
+        return checkDiag1 || checkDiag2
     }
 
     this.currentPlayerWin = () => {
-        return this.board.reduce((b, v, i) => b || this.checkRow(i) || this.checkCol(i) || this.checkDiag(i), false);
+        return this.board.reduce(
+            (b, v, i) =>
+                b || this.checkRow(i) || this.checkCol(i) || this.checkDiag(i),
+            false
+        )
     }
 
-    this.printWinner = () => document.getElementById("prompt").innerHTML = "Player " + this.currentPlayer.name + " wins";
+    this.printWinner = () =>
+        (document.getElementById('prompt').innerHTML =
+            'Player ' + this.currentPlayer.name + ' wins')
 
-    this.tie = () => this.board.reduce((b, v, i) => b && (v != "Empty"), true);
+    this.tie = () => this.board.reduce((b, v, i) => b && v != 'Empty', true)
 
-    this.printTie = () => document.getElementById("prompt").innerHTML = "Tie";
-
+    this.printTie = () => (document.getElementById('prompt').innerHTML = 'Tie')
 }
 
-function ticTackToe(id, game) {
-    let index = parseInt(id.charAt(5));
+function ticTackToe (id, game) {
+    let index = parseInt(id.charAt(5))
 
-    if (!game.isEmpty(index)) { return; }
+    if (!game.isEmpty(index)) {
+        return
+    }
 
-    game.currentPlayer.move(game, index);
+    game.currentPlayer.move(game, index)
 
-    const list = document.getElementsByClassName("button");
+    const list = document.getElementsByClassName('button')
 
     if (game.currentPlayerWin()) {
-        game.printWinner();
+        game.printWinner()
         for (i = 0; i < list.length; i++) {
-            list[i].disabled = true;
+            list[i].disabled = true
         }
         setTimeout(() => {
-            game.clean();
-            game.displayCurrentPlayer();
+            game.clean()
+            game.displayCurrentPlayer()
             for (i = 0; i < list.length; i++) {
-                list[i].disabled = false;
+                list[i].disabled = false
             }
-        }, 2000);
+        }, 2000)
     } else if (game.tie()) {
-        game.printTie();
+        game.printTie()
         for (i = 0; i < list.length; i++) {
-            list[i].disabled = true;
+            list[i].disabled = true
         }
         setTimeout(() => {
-            game.clean();
-            game.displayCurrentPlayer();
+            game.clean()
+            game.displayCurrentPlayer()
             for (i = 0; i < list.length; i++) {
-                list[i].disabled = false;
+                list[i].disabled = false
             }
-        }, 2000);
+        }, 2000)
     } else {
-        game.currentPlayer = new Player(game.currentPlayer.nextPlayer());
+        game.currentPlayer = new Player(game.currentPlayer.nextPlayer())
     }
-
 }
-
-
-
 
 window.onload = function () {
-    ttt.displayCurrentPlayer();
+    ttt.displayCurrentPlayer()
 }
 
-var startPlayer = new Player(player.X);
-var ttt = new Game(3, startPlayer);
-
-
-
-
-
-
-
-
+var startPlayer = new Player(player.X)
+var ttt = new Game(3, startPlayer)
