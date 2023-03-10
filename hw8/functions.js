e0f974d71f28cad963f69155769c73910b203ae4
swj20010308 <swj20010308@outlook.com>
2023-03-10 02:46:46 -0500

    validation check

--- hw8/functions.js
+++ hw8/functions.js
@@ -9,12 +9,23 @@ function getSkills () {
         if (request.readyState == 4 && request.status == 200) {
             var skills = JSON.parse(request.responseText);
             //alert(request.responseText);
-            alert(skills[0]['name']);
             for (let i = 0; i < skills.length; i++) {
+                let skill = skills[i];
+                document.write('<p>' + skill['name'] + '</p>');
                 document.write(
-                    '<table style="width: 100%" class="table">' +
-                        skills[i]['name']
+                    '<table style="width: 50%" id="' + skill['name'] + '">'
                 );
+                document.getElementById(skill['name']).innerHTML +=
+                    "<tr><th style='width:25%'> Level </th> <th style='width:25%'> Discription </th>";
+                for (let j = 0; j < skill['ranks'].length; j++) {
+                    let rank = skill['ranks'][j];
+                    document.getElementById(skill['name']).innerHTML +=
+                        "<tr><th style='width:25%' class='elem'>" +
+                        rank['level'] +
+                        "</th><th style='width:25%' class='elem'>" +
+                        rank['description'] +
+                        '</th></tr>';
+                }
             }
         } else {
             console.log(
