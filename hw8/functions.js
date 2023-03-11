ee2e394dc4f033a5bf19751e940012caf1db24b8
swj20010308 <swj20010308@outlook.com>
2023-03-10 20:05:42 -0500

    hw8 final

--- hw8/functions.js
+++ hw8/functions.js
@@ -1,4 +1,5 @@
 function getSkills () {
+    // normal method
     request = new XMLHttpRequest();
     request.open(
         'GET',
@@ -9,24 +10,9 @@ function getSkills () {
         if (request.readyState == 4 && request.status == 200) {
             var skills = JSON.parse(request.responseText);
             //alert(request.responseText);
-            for (let i = 0; i < skills.length; i++) {
-                let skill = skills[i];
-                document.write('<p>' + skill['name'] + '</p>');
-                document.write(
-                    '<table style="width: 50%" id="' + skill['name'] + '">'
-                );
-                document.getElementById(skill['name']).innerHTML +=
-                    "<tr><th style='width:25%'> Level </th> <th style='width:25%'> Discription </th>";
-                for (let j = 0; j < skill['ranks'].length; j++) {
-                    let rank = skill['ranks'][j];
-                    document.getElementById(skill['name']).innerHTML +=
-                        "<tr><th style='width:25%' class='elem'>" +
-                        rank['level'] +
-                        "</th><th style='width:25%' class='elem'>" +
-                        rank['description'] +
-                        '</th></tr>';
-                }
-            }
+            document.getElementById('method').innerHTML +=
+                '<h3> Normal method </h3>';
+            writeSkills(skills, 'method', '1');
         } else {
             console.log(
                 'readyState: ' +
@@ -37,4 +23,35 @@ function getSkills () {
         }
     };
     request.send();
+
+    // fetch
+    resFetch = fetch('https://mhw-db.com/skills?q={"name":"Critical Eye"}')
+        .then(res => res.text())
+        .then(s => {
+            document.getElementById('method').innerHTML += '<h3> Fetch </h3>';
+            writeSkills(JSON.parse(s), 'method', '2');
+        })
+        .catch(error => console.log(error));
+}
+
+function writeSkills (skills, id, getMethod) {
+    for (let i = 0; i < skills.length; i++) {
+        let skill = skills[i];
+        document.getElementById(id).innerHTML +=
+            '<table style="width: 50%" id="' + skill['name'] + getMethod + '">';
+        document.getElementById(skill['name'] + getMethod).innerHTML +=
+            '<tr><th style="width: 50%">' + skill['name'] + '</th></tr>';
+        document.getElementById(skill['name'] + getMethod).innerHTML +=
+            "<tr><th style='width:25%'> Level </th> <th style='width:25%'> Discription </th>";
+        for (let j = 0; j < skill['ranks'].length; j++) {
+            let rank = skill['ranks'][j];
+            document.getElementById(skill['name'] + getMethod).innerHTML +=
+                "<tr><th style='width:25%' class='elem'>" +
+                rank['level'] +
+                "</th><th style='width:25%' class='elem'>" +
+                rank['description'] +
+                '</th></tr>';
+        }
+        document.getElementById(id).innerHTML += '</table> <br>';
+    }
 }
